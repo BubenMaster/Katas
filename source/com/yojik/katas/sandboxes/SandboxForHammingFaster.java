8e6c6834d1c4d47950632eb45fa6217ea94f1a42
YojZFK <vzikich@rambler.ru>
2022-08-15 18:29:24 +0300

    solved Hamming

--- source/com/yojik/katas/sandboxes/SandboxForHammingFaster.java
+++ source/com/yojik/katas/sandboxes/SandboxForHammingFaster.java
@@ -7,7 +7,7 @@ public class SandboxForHammingFaster {
     public static void main(String[] args) {
         int n = 50;
         for (int i = 1; i<=n; i ++) {
-            System.out.println("Hamming number with index " + i + ": " +Hamming.hammingBestPractice(i));
+            System.out.println("Hamming number with index " + i + ": " +Hamming.hamming(i));
         }
 
 
